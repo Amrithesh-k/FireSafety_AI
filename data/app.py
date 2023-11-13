8f45d880bb96e6eb09450ce38c33448ae7910591
Amrithesh Kakkoth <67770915+Amrithesh-k@users.noreply.github.com>
2023-11-13 14:22:22 +0530

    Update app.py

--- data/app.py
+++ data/app.py
@@ -1,61 +1,83 @@
-
-# app.py
-from camera_input_live import camera_input_live
+import traceback
 import streamlit as st
 import cv2
 import numpy as np
 import tensorflow as tf
-import math
-import json
-from matplotlib import pyplot as plt
-# from main import create_model, NUM_CLASSES, IMG_SIZE, NUM_EPOCHS, TRAIN_BATCH_SIZE, TEST_BATCH_SIZE, prepare_image_for_prediction, get_display_string, draw_prediction, label_dict_l, predict, train_model, get_label_dict, train_generator, validation_generator
-from main import prepare_image_for_prediction, get_display_string, draw_prediction
+from tensorflow.keras.applications.resnet50 import preprocess_input
+
 # Load the trained model
 trained_model_l = tf.keras.models.load_model("trained_model_l.h5")
 
 # Load the label dictionary
-with open("label_dict_l.json", "r") as f:
-    temp = json.load(f)
-    python_dict = json.loads(temp)
+label_dict = {0: "default", 1: "fire", 2: "smoke"}
 
-video_capture = cv2.VideoCapture(0)
-st_image = st.empty()
+IMG_SIZE = 224
 
-IMG_SIZE = 255
+
+def draw_prediction(frame, class_string):
+    x_start = frame.shape[1] - 600
+    cv2.putText(frame, class_string, (x_start, 75),
+                cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 0, 0), 2, cv2.LINE_AA)
+    return frame
+
+
+def get_display_string(pred_class, label_dict):
+    txt = ""
+    for c, confidence in pred_class:
+        label = label_dict.get(int(c), "Unknown")
+        txt += label
+        if int(c):
+            txt += '[' + str(confidence) + ']'
+    return txt
+
+
+def prepare_image_for_prediction(img):
+    img = np.expand_dims(img, axis=0)
+    return preprocess_input(img)
 
 
 def real_time_detection(video_capture, st_image):
-    while st.checkbox("Enable Real-time Detection"):
-        ret_val, frame = video_capture.read()
-        print("casse1")
+    try:
+        while True:
+            ret_val, frame = video_capture.read()
 
-        resized_frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
-        print("casse2")
+            # Ensure the frame is successfully captured
+            if not ret_val:
+                st.warning("Error: Unable to capture frame.")
+                break
 
-        frame_for_pred = prepare_image_for_prediction(resized_frame)
-        print("3")
-        # Use the loaded model for prediction
-        pred_vec = trained_model_l.predict(frame_for_pred)
-        print("4")
-        pred_class = []
-        print("5")
-        confidence = np.round(pred_vec.max(), 2)
-        print("6")
-        if confidence > 0.4:
-            pc = pred_vec.argmax()
-            pred_class.append((pc, confidence))
+            resized_frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+            frame_for_pred = prepare_image_for_prediction(resized_frame)
 
-            print("7")
-        else:
-            pred_class.append((0, 0))
-            print("8")
-        if pred_class:
-            txt = get_display_string(pred_class, python_dict)
-            frame = draw_prediction(frame, txt)
-            print("9")
+            frame_for_pred = np.squeeze(frame_for_pred, axis=0)
 
-        st_image.image(frame, channels="BGR", use_column_width=True)
-        print("10")
+            pred_vec = trained_model_l.predict(
+                np.expand_dims(frame_for_pred, axis=0))
+
+            pred_class = []
+
+            confidence = np.round(pred_vec.max(), 2)
+
+            if confidence > 0.4:
+                pc = np.argmax(pred_vec)
+                pred_class.append((pc, confidence))
+            else:
+                pred_class.append((0, 0))
+
+            if pred_class:
+                txt = get_display_string(pred_class, label_dict)
+                frame = draw_prediction(frame, txt)
+
+            st_image.image(frame, channels="BGR", use_column_width=True)
+
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        traceback.print_exc()
 
 
-real_time_detection(video_capture, st_image)
+if __name__ == "__main__":
+    # Assuming video_capture and st_image are defined before calling real_time_detection
+    video_capture = cv2.VideoCapture(0)
+    st.title("Real-time Detection App")
+    st_image = st.empty()
+    real_time_detection(video_capture, st_image)
