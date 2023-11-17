1032fb80b3923018dd7ee976372cd1211251d187
Amrithesh Kakkoth <67770915+Amrithesh-k@users.noreply.github.com>
2023-11-17 17:02:38 +0530

    Update app.py

--- data/app.py
+++ data/app.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
 # Load the trained model
-trained_model_l = tf.keras.models.load_model("/workspaces/FireSafety_AI/data/trained_model_l.h5")
+trained_model_l = tf.keras.models.load_model("/mount/src/firesafety_ai/data/trained_model_l.h5")
 
 # Load the label dictionary
 label_dict = {0: "default", 1: "fire", 2: "smoke"}
